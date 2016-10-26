6db698ce053cb2b06a6c80fe5a5a2334fa3b2389
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2016-10-26 15:40:08 +0200

    HIPCMS-89 added a shortcut s.t. no authorization is required when in development mode

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
  *
  * (c) Copyright Ascensio System Limited 2010-2016
  *
- * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU 
- * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html). 
- * In accordance with Section 7(a) of the GNU GPL its Section 15 shall be amended to the effect that 
+ * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html).
+ * In accordance with Section 7(a) of the GNU GPL its Section 15 shall be amended to the effect that
  * Ascensio System SIA expressly excludes the warranty of non-infringement of any third-party rights.
  *
  * THIS PROGRAM IS DISTRIBUTED WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR
@@ -12,13 +12,13 @@
  *
  * You can contact Ascensio System SIA by email at sales@onlyoffice.com
  *
- * The interactive user interfaces in modified source and object code versions of ONLYOFFICE must display 
+ * The interactive user interfaces in modified source and object code versions of ONLYOFFICE must display
  * Appropriate Legal Notices, as required under Section 5 of the GNU GPL version 3.
  *
- * Pursuant to Section 7 § 3(b) of the GNU GPL you must retain the original ONLYOFFICE logo which contains 
- * relevant author attributions when distributing the software. If the display of the logo in its graphic 
- * form is not reasonably feasible for technical reasons, you must include the words "Powered by ONLYOFFICE" 
- * in every copy of the program you distribute. 
+ * Pursuant to Section 7 § 3(b) of the GNU GPL you must retain the original ONLYOFFICE logo which contains
+ * relevant author attributions when distributing the software. If the display of the logo in its graphic
+ * form is not reasonably feasible for technical reasons, you must include the words "Powered by ONLYOFFICE"
+ * in every copy of the program you distribute.
  * Pursuant to Section 7 § 3(e) we decline to grant you any rights under trademark law for use of our trademarks.
  *
 */
@@ -82,30 +82,34 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(__dirname + "/public/images/favicon.ico"));
 
 app.use(function(req, res, next) {
-    var token = req.get('Authorization');
-    var decoded_token = jwt.decode(token, {complete: true});
-    var kid = decoded_token.header.kid;
-    var issuer = decoded_token.payload.iss;
+    if (process.env.NODE_ENV === 'production') {
+      next(); // skip authorization if in production mode
+    } else {
+      var token = req.get('Authorization');
+      var decoded_token = jwt.decode(token, {complete: true});
+      var kid = decoded_token.header.kid;
+      var issuer = decoded_token.payload.iss;
 
-    var url = issuer + '.well-known/jwks';
+      var url = issuer + '.well-known/jwks';
 
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
+      request({
+          url: url,
+          json: true
+      }, function (error, response, body) {
 
-        if (!error && response.statusCode === 200) {
-            var jwk = jwksUtils.findJWK(kid, body);
+          if (!error && response.statusCode === 200) {
+              var jwk = jwksUtils.findJWK(kid, body);
 
-            if(jws.verify(token, jwk)) {
-                console.log("client authentificated");
-                next()
-            } else {
-                throw new Error();
-            }
+              if(jws.verify(token, jwk)) {
+                  console.log("client authentificated");
+                  next();
+              } else {
+                  throw new Error();
+              }
 
-        }
-    });
+          }
+      });
+    }
 
 });
 
@@ -443,7 +447,7 @@ app.get("/editor", function (req, res) {
         fileName = fileUtility.getFileName(req.query.fileName);
         var key = docManager.getKey(fileName);
         var url = docManager.getFileUri(fileName);
-        var mode = req.query.mode || "edit"; //mode: view/edit 
+        var mode = req.query.mode || "edit"; //mode: view/edit
         var type = req.query.type || "desktop"; //type: embedded/mobile/desktop
         var canEdit = configServer.get('editedDocs').indexOf(fileUtility.getFileExtension(fileName)) != -1;
 
